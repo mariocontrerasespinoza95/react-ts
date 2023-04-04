c923f1595656c48cfa7253f0c0446aa7ec73ca4a
Mario Contreras <marioce751@gmail.com>
2023-04-03 19:52:17 -0700

    Added react-icons

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,33 +1,10 @@
-import { useState } from 'react';
-import Alert from './components/Alert';
-import Button from './components/Button';
-import ListGroup from './components/ListGroup';
-import './App.css';
+import { BsFillCalendarFill } from 'react-icons/bs';
 
 function App() {
-    const [alertVisible, setAlertVisibility] = useState(false);
-    const items = ['New York', 'San Francisco', 'Tokyo', 'Paris'];
-    const heading = 'Cities';
-    const handleSelectItem = (item: string) => {
-        console.log(item);
-    };
-
     return (
-        <div>
-            {alertVisible && (
-                <Alert onClose={() => setAlertVisibility(false)}>
-                    My Alert
-                </Alert>
-            )}
-            <Button color="primary" onClick={() => setAlertVisibility(true)}>
-                My Button
-            </Button>
-            <ListGroup
-                items={items}
-                heading={heading}
-                onSelectItem={handleSelectItem}
-            ></ListGroup>
-        </div>
+        <>
+            <BsFillCalendarFill color="red" size={40} />
+        </>
     );
 }
 
