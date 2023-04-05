e03ce71fe899b7c11bbd160a395bf31cdf4e2be7
Mario Contreras <marioce751@gmail.com>
2023-04-05 14:44:44 -0700

    Created form and using useRef

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,14 +1,10 @@
 import { useState } from 'react';
-import Like from './components/Like';
-import NavBar from './components/NavBar';
-import Cart from './components/Cart';
+import Form from './components/Form';
 
 function App() {
-    const [cartItems, setCartItems] = useState(['Product 1', 'Product 2']);
     return (
         <div>
-            <NavBar cartItemsCount={cartItems.length} />
-            <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+            <Form />
         </div>
     );
 }
---
+++ react-app/src/components/Form.tsx
@@ -0,0 +1,46 @@
+import { FormEvent, useRef } from 'react';
+
+const Form = () => {
+    const nameRef = useRef<HTMLInputElement>(null);
+    const ageRef = useRef<HTMLInputElement>(null);
+    const person = { name: '', age: 0 };
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        if (nameRef.current) person.name = nameRef.current.value;
+        if (ageRef.current) person.age = parseInt(ageRef.current.value);
+        console.log(person);
+    };
+
+    return (
+        <form onSubmit={handleSubmit}>
+            <div className="mb-3">
+                <label htmlFor="name" className="form-label">
+                    Name
+                </label>
+                <input
+                    id="name"
+                    type="text"
+                    className="form-control"
+                    ref={nameRef}
+                />
+            </div>
+            <div className="mb-3">
+                <label htmlFor="age" className="form-label">
+                    Age
+                </label>
+                <input
+                    id="age"
+                    type="number"
+                    className="form-control"
+                    ref={ageRef}
+                />
+            </div>
+            <button className="btn btn-primary" type="submit">
+                Submit
+            </button>
+        </form>
+    );
+};
+
+export default Form;
