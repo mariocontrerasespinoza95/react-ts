150793176476086fe5d4408e2dc03da302d2ed1e
Mario Contreras <marioce751@gmail.com>
2023-04-05 14:29:28 -0700

    Added NavBar and Cart comps

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,10 +1,15 @@
+import { useState } from 'react';
 import Like from './components/Like';
+import NavBar from './components/NavBar';
+import Cart from './components/Cart';
 
 function App() {
+    const [cartItems, setCartItems] = useState(['Product 1', 'Product 2']);
     return (
-        <>
-            <Like onClick={() => console.log('clicked')}></Like>
-        </>
+        <div>
+            <NavBar cartItemsCount={cartItems.length} />
+            <Cart cartItems={cartItems} onClear={() => setCartItems([])} />
+        </div>
     );
 }
 
