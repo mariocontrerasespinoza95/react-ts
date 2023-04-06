c7a190b6d34058ec706ddf180667bd794c098c60
Mario Contreras <marioce751@gmail.com>
2023-04-06 12:06:46 -0700

    Added a useEffect with a destruction function for examples

--- react-app/src/App.tsx
+++ react-app/src/App.tsx
@@ -1,22 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import ProductList from './components/ProductList';
 
-function App() {
-    const [category, setCategory] = useState('');
+const connect = () => console.log('Connecting');
+const disconnect = () => console.log('Disconnecting');
 
-    return (
-        <div>
-            <select
-                className="form-select"
-                onChange={(event) => setCategory(event.target.value)}
-            >
-                <option value=""></option>
-                <option value="Clothing">Clothing</option>
-                <option value="Household">Household</option>
-            </select>
-            <ProductList category={category} />
-        </div>
-    );
+function App() {
+    useEffect(() => {
+        connect();
+
+        return () => disconnect();
+    });
+
+    return <div></div>;
 }
 
 export default App;
