1feb71ed9da61c2df257a4083c6e3d3358cd95dd
Mario Contreras <marioce751@gmail.com>
2023-04-08 20:07:49 -0700

    Adjusted skeleton size

--- game-hub/src/components/GameCardSkeleton.tsx
+++ game-hub/src/components/GameCardSkeleton.tsx
@@ -2,13 +2,8 @@ import { Card, CardBody, Skeleton, SkeletonText } from '@chakra-ui/react';
 
 const GameCardSkeleton = () => {
     return (
-        <Card
-            borderRadius={10}
-            width={'453.44px'}
-            height={'407.09px'}
-            overflow={'hidden'}
-        >
-            <Skeleton height={'200px'} />
+        <Card borderRadius={10} width={'300px'} overflow={'hidden'}>
+            <Skeleton height="200px" />
             <CardBody>
                 <SkeletonText />
             </CardBody>
