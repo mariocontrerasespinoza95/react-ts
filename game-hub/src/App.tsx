fb885464af0f4244723c1f38d72dcbad4edffbf3
Mario Contreras <marioce751@gmail.com>
2023-04-10 01:51:36 -0700

    Removed unused imports

--- game-hub/src/App.tsx
+++ game-hub/src/App.tsx
@@ -1,13 +1,13 @@
-import { Box, Flex, Grid, GridItem, HStack, Show } from '@chakra-ui/react';
+import { Box, Flex, Grid, GridItem, Show } from '@chakra-ui/react';
 import { useState } from 'react';
 import GameGrid from './components/GameGrid';
+import GameHeading from './components/GameHeading';
 import GenreList from './components/GenreList';
 import NavBar from './components/NavBar';
 import PlatformSelector from './components/PlatformSelector';
 import SortSelector from './components/SortSelector';
 import { Platform } from './hooks/useGames';
 import { Genre } from './hooks/useGenres';
-import GameHeading from './components/GameHeading';
 
 export interface GameQuery {
     genre: Genre | null;
