5fbd143625f206e64da504798ec80df735526664
Mario Contreras <marioce751@gmail.com>
2023-09-07 10:55:01 -0700

    Refactor - move entitiy interfaces

--- game-hub/src/components/PlatformSelector.tsx
+++ game-hub/src/components/PlatformSelector.tsx
@@ -1,7 +1,7 @@
 import { Button, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react';
 import { BsChevronDown } from 'react-icons/bs';
-import usePlatforms, { Platform } from '../hooks/usePlatforms';
 import usePlatform from '../hooks/usePlatform';
+import usePlatforms from '../hooks/usePlatforms';
 import useGameQueryStore from '../store';
 
 const PlatformSelector = () => {
