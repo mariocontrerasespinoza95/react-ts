6bef29ef0a6933dab0a1a9ec7fd63ea7483f541d
Mario Contreras <marioce751@gmail.com>
2023-09-07 20:10:06 -0700

    Refactor entities interfaces

--- game-hub/src/components/GameCard.tsx
+++ game-hub/src/components/GameCard.tsx
@@ -1,5 +1,5 @@
 import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react';
-import { Game } from '../entities/Game';
+import Game from '../entities/Game';
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
@@ -17,7 +17,9 @@ const GameCard = ({ game }: Props) => {
             <CardBody>
                 <HStack justifyContent={'space-between'} marginBottom={3}>
                     <PlatformIconList
-                        platforms={game.parent_platforms?.map((m) => m.platform)}
+                        platforms={game.parent_platforms?.map(
+                            (m) => m.platform
+                        )}
                     />
                     <CriticScore score={game.metacritic} />
                 </HStack>
